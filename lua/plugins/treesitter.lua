9f0af8fc6969e0e83c3b0f8046b6aae2ce4eb855
Alexander Muratov <muratoffalex@gmail.com>
2023-11-22 01:08:25 +0500

    set correct keymaps for treesitter incremental_selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -50,10 +50,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
+            init_selection = '<A-Up>',
+            node_incremental = '<A-Up>',
+            scope_incremental = '<leader>ss', -- TODO: add keybind for scope_incremental
+            node_decremental = '<A-Down>',
           },
         },
 
