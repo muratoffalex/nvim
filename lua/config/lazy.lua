15fbbc2883db0a2b7f4aa476a77ed43dd1a791eb
Alexander Muratov <muratoffalex@gmail.com>
2023-12-14 23:59:11 +0500

    fix: optimize lazy.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,13 +5,44 @@ local plugins = {
 }
 
 local options = {
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false,
+  },
   performance = {
     cache = {
-      enabled = false,
+      enabled = true,
     },
     rtp = {
       disabled_plugins = {
-          "netrw",
+        '2html_plugin',
+        'tohtml',
+        'getscript',
+        'getscriptPlugin',
+        'gzip',
+        'logipat',
+        'netrw',
+        'netrwPlugin',
+        'netrwSettings',
+        'netrwFileHandlers',
+        -- "matchit",
+        'tar',
+        'tarPlugin',
+        'rrhelper',
+        'spellfile_plugin',
+        'vimball',
+        'vimballPlugin',
+        'zip',
+        'zipPlugin',
+        -- "tutor",
+        'rplugin',
+        'syntax',
+        'synmenu',
+        'optwin',
+        'compiler',
+        'bugreport',
+        'ftplugin',
       },
     },
   },
@@ -25,7 +56,7 @@ if not vim.loop.fs_stat(lazypath) then
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',   -- latest stable release
     lazypath,
   }
 end
