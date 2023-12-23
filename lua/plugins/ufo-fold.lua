169b337cfd280edb3c56e840162d844a578f7863
Alexander Muratov <muratoffalex@gmail.com>
2023-12-23 21:14:00 +0500

    fix(nvim-ufo): fix argument typo

--- lua/plugins/ufo-fold.lua
+++ lua/plugins/ufo-fold.lua
@@ -3,7 +3,7 @@ return {
   enabled = true,
   event = 'VeryLazy',
   init = function()
-    vim.o.foldcolumn = 0
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
@@ -17,19 +17,19 @@ return {
   },
   dependencies = {
     'kevinhwang91/promise-async',
-    -- {
-    --   'luukvbaal/statuscol.nvim',
-    --   config = function()
-    --     local builtin = require 'statuscol.builtin'
-    --     require('statuscol').setup {
-    --       relculright = true,
-    --       segments = {
-    --         { text = { '%s' },                  click = 'v:lua.ScSa' },
-    --         { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
-    --         { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
-    --       },
-    --     }
-    --   end,
+    --  {
+    --    'luukvbaal/statuscol.nvim',
+    --    config = function()
+    --      local builtin = require 'statuscol.builtin'
+    --      require('statuscol').setup {
+    --        relculright = true,
+    --        segments = {
+    --          { text = { '%s' },                  click = 'v:lua.ScSa' },
+    --          { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+    --          { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+    --        },
+    --      }
+    --    end,
     -- },
   },
 }
