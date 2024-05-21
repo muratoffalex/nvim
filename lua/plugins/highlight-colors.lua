10b6565f738579271418a32f7e046a5c253b2516
Alexander Muratov <muratoffalex@gmail.com>
2024-05-21 22:56:00 +0500

    feat: replace colorizer with nvim-highlight-colors

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,15 @@
+-- differences from nvchad colorizer:
+-- faster and simpler
+-- supports tailwind colors, rgb, hsl etc.
+-- supports rendering in background, foreground and virtual text
+return {
+   'brenoprata10/nvim-highlight-colors',
+   event = 'VeryLazy',
+   config = function()
+      require('nvim-highlight-colors').setup {
+         render = "background",
+         virtual_symbol = '⏺︎',
+         enable_tailwind = true,
+      }
+   end,
+}
