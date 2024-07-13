ff7017618c3dfea660f67f95bbb21b6606959e9a
Alexander Muratov <muratoffalex@gmail.com>
2024-07-13 16:28:33 +0500

    chore(which-key): upgrade to v3

--- lua/plugins/actions-preview.lua
+++ lua/plugins/actions-preview.lua
@@ -2,6 +2,8 @@ return {
    'aznhe21/actions-preview.nvim',
    config = function()
       vim.keymap.set({ 'v', 'n' }, '<leader>ca', require('actions-preview').code_actions)
-      require('which-key').register({ ['<leader>ca'] = 'Code actions' }, { mode = {'n', 'v'} })
+      require('which-key').add({
+       { "<leader>ca", desc = "Code actions", mode = { "n", "v" } },
+      })
    end,
 }
