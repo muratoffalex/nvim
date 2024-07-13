aaf433b6d00d7f8787478dbc7e09fceb5acd7ff0
Alexander Muratov <muratoffalex@gmail.com>
2024-07-13 16:30:17 +0500

    chore(trouble.nvim): upgrade to v3

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,14 +1,15 @@
 return {
    'folke/trouble.nvim',
    dependencies = { 'nvim-tree/nvim-web-devicons' },
+   cmd = 'Trouble',
    keys = {
-      { '<leader>tg', "<cmd>lua require('trouble').toggle()<CR>", desc = 'Trouble toggle' },
-      { '<leader>tw', "<cmd>lua require('trouble').toggle('workspace_diagnostics')<CR>", desc = 'Trouble toggle workspace diagnostic' },
-      { '<leader>tc', "<cmd>lua require('trouble').toggle('document_diagnostics')<CR>", desc = 'Trouble toggle document diagnostic' },
-      { '<leader>tq', "<cmd>lua require('trouble').toggle('quickfix')<CR>", desc = 'Trouble toggle quickfix list' },
-      { '<leader>tl', "<cmd>lua require('trouble').toggle('loclist')<CR>", desc = 'Trouble toggle loclist' },
-      { 'gr', "<cmd>lua require('trouble').toggle('lsp_references')<CR>", desc = 'Trouble lsp references' },
-      { 'gi', "<cmd>lua require('trouble').toggle('lsp_implementations')<CR>", desc = 'Trouble lsp implementations' },
+      { '<leader>tg', "<cmd>Trouble toggle<CR>", desc = 'Trouble toggle' },
+      { '<leader>tw', "<cmd>Trouble diagnostics toggle<CR>", desc = 'Trouble toggle workspace diagnostic' },
+      { '<leader>tc', "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = 'Trouble toggle document diagnostic' },
+      { '<leader>tq', "<cmd>Trouble qflist toggle<CR>", desc = 'Trouble toggle quickfix list' },
+      { '<leader>tl', "<cmd>Trouble loclist toggle<CR>", desc = 'Trouble toggle loclist' },
+      { 'gr', "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", desc = 'Trouble lsp references/definitions/...' },
+      -- { 'gi', "<cmd>lua require('trouble').toggle('lsp_implementations')<CR>", desc = 'Trouble lsp implementations' },
    },
    opts = {},
 }
