7177b31a18eff795c6eb322f5fae02fc38f700c5
Alexander Muratov <muratoffalex@gmail.com>
2024-07-13 16:32:12 +0500

    chore(persistence.nvim): upgrade to v3

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,7 +2,9 @@ return {
    'folke/persistence.nvim',
    event = 'BufReadPre',
    opts = {
-      options = vim.opt.sessionoptions:get(),
+      dir = vim.fn.stdpath 'state' .. '/sessions/',
+      need = 1,
+      branch = true, -- use git branch to save session
       pre_save = function()
          vim.cmd 'silent! Neotree close'
          vim.cmd 'silent! DBUIClose'
@@ -12,12 +14,12 @@ return {
          vim.cmd 'silent! DiffviewClose'
          vim.cmd 'silent! lua require("nvterm.terminal").close_all_terms()'
       end, -- a function to call before saving the session
-      save_empty = false, -- don't save if there are no open file buffers
    },
-  -- stylua: ignore
-  keys = {
-    { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
-  },
+   -- stylua: ignore
+   keys = {
+      { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+      { "<leader>qS", function() require("persistence").select() end,              desc = "Select Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+   },
 }
