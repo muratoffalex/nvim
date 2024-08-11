9c7b05604f064434721e2c1c44d0f41ea2042042
Alexander Muratov <muratoffalex@gmail.com>
2024-08-11 18:10:40 +0500

    chore: move nvim-notify to separated file

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,39 +6,36 @@ return {
          enabled = true,
          view = 'notify',
       },
+      lsp = {
+         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+         override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+         },
+      },
+      presets = {
+         bottom_search = true,         -- use a classic bottom cmdline for search
+         command_palette = true,       -- position the cmdline and popupmenu together
+         long_message_to_split = true, -- long messages will be sent to a split
+         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+         lsp_doc_border = false,       -- add a border to hover docs and signature help
+      },
    },
    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
    },
    keys = {
-      { '<leader>nd', '<cmd>NoiceDismiss<cr>', desc = 'Noice dismiss' }
+      { '<leader>nd', '<cmd>NoiceDismiss<cr>', desc = 'Noice dismiss' },
    },
-   init = function()
-      vim.notify = require 'notify'
-   end,
-   config = function()
-      require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      })
+   config = function(_, opts)
+      -- ref: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/ui.lua
+      -- HACK: noice shows messages from before it was enabled,
+      -- but this is not ideal when Lazy is installing plugins,
+      -- so clear the messages in this case.
+      if vim.o.filetype == 'lazy' then
+         vim.cmd [[messages clear]]
+      end
+      require('noice').setup(opts)
    end,
 }
