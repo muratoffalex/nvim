18af53b18a002de7f7c361b95def6a2ac3554931
Alexander Muratov <muratoffalex@gmail.com>
2024-11-12 00:49:26 +0500

    chore(plugins): update snipe select ui keymaps

--- lua/muratoffalex/plugins/snipe.lua
+++ lua/muratoffalex/plugins/snipe.lua
@@ -8,7 +8,7 @@ return {
    opts = {
       ui = {
          position = 'cursor',
-         max_path_width = 3
+         max_path_width = 3,
       },
       sort = 'last',
       hints = {
@@ -21,15 +21,17 @@ return {
       snipe.setup(opts)
       vim.keymap.set('n', 'tt', snipe.open_buffer_menu)
 
-      snipe.ui_select_menu = require("snipe.menu"):new({
+      snipe.ui_select_menu = require('snipe.menu'):new {
          position = opts.ui.position,
          dictionary = opts.hints.dictionary,
-      })
-      snipe.ui_select_menu:add_new_buffer_callback(function (m)
-        vim.keymap.set("n", "<esc>", function ()
-          m:close()
-        end, { nowait = true, buffer = m.buf })
+      }
+      snipe.ui_select_menu:add_new_buffer_callback(function(m)
+         local function close_menu()
+            m:close()
+         end
+         vim.keymap.set('n', '<esc>', close_menu, { nowait = true, buffer = m.buf })
+         vim.keymap.set('n', 'q', close_menu, { nowait = true, buffer = m.buf })
       end)
-      vim.ui.select = snipe.ui_select;
+      vim.ui.select = snipe.ui_select
    end,
 }
