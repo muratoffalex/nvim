f9e1c04f6b3deebdb861ab9f79e7ba855c08f094
Alexander Muratov <muratoffalex@gmail.com>
2024-12-08 15:51:13 +0500

    chore: update snipe, add dressing integration

--- lua/muratoffalex/plugins/snipe.lua
+++ lua/muratoffalex/plugins/snipe.lua
@@ -1,47 +1,42 @@
 return {
   {
-    'leath-dub/snipe.nvim',
-    lazy = false,
-    keys = {
-      { 'tt', desc = 'Toggle buffer menu' },
+    'stevearc/dressing.nvim',
+    dependencies = 'leath-dub/snipe.nvim',
+    opts = {
+      select = {
+        enabled = true,
+        backend = 'snipe',
+      },
     },
-    priority = 10, -- because it needs to be loaded after dressing plugin
+  },
+  {
+    'leath-dub/snipe.nvim',
+    keys = {
+      {
+        'tt',
+        function()
+          require('snipe').open_buffer_menu()
+        end,
+        desc = 'Toggle buffer menu',
+      },
+    },
     opts = {
       ui = {
         position = 'cursor',
-        max_path_width = 3,
       },
       sort = 'last',
       hints = {
         -- specified for colemak layout
         dictionary = 'arstdwfneiohmpg',
       },
-    },
-    config = function(_, opts)
-      local snipe = require 'snipe'
-      snipe.setup(opts)
-      vim.keymap.set('n', 'tt', snipe.open_buffer_menu)
-
-      snipe.ui_select_menu = require('snipe.menu'):new {
-        position = opts.ui.position,
-        dictionary = opts.hints.dictionary,
-      }
-      snipe.ui_select_menu:add_new_buffer_callback(function(m)
-        local function close_menu()
-          m:close()
-        end
-        vim.keymap.set('n', '<esc>', close_menu, { nowait = true, buffer = m.buf })
-        vim.keymap.set('n', 'q', close_menu, { nowait = true, buffer = m.buf })
-      end)
-      vim.ui.select = snipe.ui_select
-    end,
+    }
   },
   {
     'kungfusheep/snipe-spell.nvim',
     dependencies = 'leath-dub/snipe.nvim',
     config = true,
     keys = {
-      { 'z=', '<cmd>SnipeSpell<cr>', desc = 'Snipe Spellchecker' },
+      { '<leader>sc', '<cmd>SnipeSpell<cr>', desc = 'Snipe Spellchecker' },
     },
-  }
+  },
 }
