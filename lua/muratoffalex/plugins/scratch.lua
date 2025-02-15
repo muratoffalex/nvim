e7c9047359ed59c2a954cefa899d51828724c955
Alexander Muratov <muratoffalex@gmail.com>
2025-02-15 15:21:56 +0500

    feat: add scratch plugin

---
+++ lua/muratoffalex/plugins/scratch.lua
@@ -0,0 +1,8 @@
+return {
+  'LintaoAmons/scratch.nvim',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>sn', '<cmd>ScratchWithName<cr>', desc = 'Scratch new' },
+    { '<leader>so', '<cmd>ScratchOpen<cr>', desc = 'Scratch open' },
+  },
+}
