9e03cf2809eda3603a8e804e4356fd35c314e351
Alexander Muratov <muratoffalex@gmail.com>
2025-03-15 16:58:49 +0500

    chore: delete old themes

--- lua/muratoffalex/plugins/themes/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  enabled = require('muratoffalex.config').theme == 'catppuccin',
-  config = function()
-    -- vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
-}
