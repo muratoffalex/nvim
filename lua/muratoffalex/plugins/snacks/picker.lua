75e1ff82d8fb81135d31c5b8948d04c7649c4a31
Alexander Muratov <muratoffalex@gmail.com>
2025-03-16 20:54:45 +0500

    feat: set snacks picker as vim.ui.select

--- lua/muratoffalex/plugins/snacks/picker.lua
+++ lua/muratoffalex/plugins/snacks/picker.lua
@@ -1,5 +1,11 @@
 return {
   'folke/snacks.nvim',
+  opts = {
+    picker = {
+      layout = 'ivy',
+      ui_select = true,
+    },
+  },
   keys = {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
@@ -52,15 +58,11 @@ return {
     -- LSP
     { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    -- references in trouble.nvim
     -- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-  },
-  opts = {
-    picker = {
-      layout = 'ivy',
-    },
-  },
+  }
 }
