7dc6802c18517a24c3eb6314bec211dc8f41890c
Alexander Muratov <muratoffalex@gmail.com>
2025-04-05 23:25:01 +0500

    feat(lualine): add codecompanion custom extension

--- lua/muratoffalex/plugins/lualine.lua
+++ lua/muratoffalex/plugins/lualine.lua
@@ -25,6 +25,26 @@ local mode = {
   end,
 }
 
+local codecompanion = require("codecompanion")
+
+local function codecompanion_adapter_name()
+  local chat = codecompanion.buf_get_chat(vim.api.nvim_get_current_buf())
+  if not chat then
+    return nil
+  end
+
+  return " " .. chat.adapter.formatted_name -- or just name
+end
+
+local function codecompanion_current_model_name()
+  local chat = codecompanion.buf_get_chat(vim.api.nvim_get_current_buf())
+  if not chat then
+    return nil
+  end
+
+  return chat.settings.model
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
@@ -121,6 +141,40 @@ return {
         'trouble',
         'mason',
         'symbols-outline',
+        "quickfix",
+        {
+          filetypes = { "codecompanion" },
+          sections = {
+            lualine_a = {
+              mode,
+            },
+            lualine_b = {
+              codecompanion_adapter_name,
+            },
+            lualine_c = {
+              codecompanion_current_model_name,
+            },
+            lualine_x = {},
+            lualine_y = {
+              "progress",
+            },
+            lualine_z = {
+              "location",
+            },
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {
+              codecompanion_adapter_name,
+            },
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {
+              "progress",
+            },
+            lualine_z = {},
+          },
+        },
       },
     }
   end,
