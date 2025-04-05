2738ca4edf51cfeeb2e245bfcb9c26b3c4738eb4
Alexander Muratov <muratoffalex@gmail.com>
2025-04-05 23:22:06 +0500

    chore(lualine): remove xcode related components

--- lua/muratoffalex/plugins/lualine.lua
+++ lua/muratoffalex/plugins/lualine.lua
@@ -19,32 +19,6 @@ return {
       return utils.dump(clients_list, '·')
     end
 
-    local function xcodebuild_device()
-      if vim.g.xcodebuild_platform == 'macOS' then
-        return ' macOS'
-      end
-
-      local deviceIcon = ''
-      if vim.g.xcodebuild_platform:match 'watch' then
-        deviceIcon = '􀟤'
-      elseif vim.g.xcodebuild_platform:match 'tv' then
-        deviceIcon = '􀡴 '
-      elseif vim.g.xcodebuild_platform:match 'vision' then
-        deviceIcon = '􁎖 '
-      end
-
-      if vim.g.xcodebuild_os then
-        return deviceIcon .. ' ' .. vim.g.xcodebuild_device_name .. ' (' .. vim.g.xcodebuild_os .. ')'
-      end
-
-      return deviceIcon .. ' ' .. vim.g.xcodebuild_device_name
-    end
-
-    local xcodebuild_build_status = {
-      "' ' .. vim.g.xcodebuild_last_status",
-      color = { fg = 'Gray' },
-    }
-
     local copilot = {
       'copilot',
       symbols = {
@@ -94,8 +68,6 @@ return {
           'pretty_path'
         },
         lualine_x = {
-          xcodebuild_build_status,
-          xcodebuild_device,
           copilot,
           {
             lsp_clients,
