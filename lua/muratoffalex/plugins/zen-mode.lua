e6c078fff636c66d90aa56887972fe6115536632
Alexander Muratov <muratoffalex@gmail.com>
2025-04-05 17:05:26 +0500

    refactor(zen-mode): simplify configuration and disable twilight

--- lua/muratoffalex/plugins/zen-mode.lua
+++ lua/muratoffalex/plugins/zen-mode.lua
@@ -4,9 +4,6 @@ return {
   keys = {
     { '<leader>mz', '<cmd>ZenMode<cr>', desc = 'Zen Mode' },
   },
-  dependencies = {
-    'folke/twilight.nvim',
-  },
   opts = {
     window = {
       backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
@@ -14,18 +11,18 @@ return {
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 86, -- width of the Zen window
+      width = 83, -- width of the Zen window
 
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
       options = {
-        -- signcolumn = "no", -- disable signcolumn
+        signcolumn = 'no', -- disable signcolumn
         -- number = false, -- disable number column
         -- relativenumber = false, -- disable relative numbers
-        -- cursorline = false, -- disable cursorline
-        -- cursorcolumn = false, -- disable cursor column
-        -- foldcolumn = "0", -- disable fold column
-        -- list = false, -- disable whitespace characters
+        cursorline = false, -- disable cursorline
+        cursorcolumn = false, -- disable cursor column
+        foldcolumn = '0', -- disable fold column
+        list = false, -- disable whitespace characters
       },
     },
     plugins = {
@@ -39,7 +36,7 @@ return {
         -- statusline will be shown only if 'laststatus' == 3
         laststatus = 0, -- turn off the statusline in zen mode
       },
-      twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+      twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
       gitsigns = { enabled = true }, -- disables git signs
       tmux = { enabled = true }, -- disables the tmux statusline
       diagnostics = { enabled = true }, -- disables diagnosticls signs
@@ -51,15 +48,10 @@ return {
         enabled = true,
         font = '+4', -- font size increment
       },
-      -- this will change the font size on alacritty when in zen mode
-      -- requires  Alacritty Version 0.10.0 or higher
-      -- uses `alacritty msg` subcommand to change font size
       alacritty = {
         enabled = true,
         font = '22', -- font size
       },
-      -- this will change the font size on wezterm when in zen mode
-      -- See alse also the Plugins/Wezterm section in this projects README
       wezterm = {
         enabled = true,
         -- can be either an absolute font size or the number of incremental steps
