bf8a25c3c38bf8ded1b7feb7de669169f74b56c7
Alexander Muratov <muratoffalex@gmail.com>
2025-04-06 19:44:09 +0500

    feat(avante): add deepseek provider

--- lua/muratoffalex/plugins/avante.lua
+++ lua/muratoffalex/plugins/avante.lua
@@ -3,32 +3,40 @@ return {
   version = false, -- set this if you want to always pull the latest change
   lazy = true,
   event = 'VeryLazy',
-  -- event = {
-  --   {
-  --     event = 'ModeChanged',
-  --     pattern = { '*:[vV\x16]*' }, -- when enter in visual mode
-  --   },
-  -- },
   keys = {
     { '<leader>aa', '<cmd>AvanteAsk<cr>' },
     { '<leader>at', '<cmd>AvanteToggle<cr>' },
     { '<leader>ac', '<cmd>AvanteChat<cr>' },
     { '<leader>aC', '<cmd>AvanteClear<cr>' },
+    { '<leader>am', '<cmd>AvanteModels<cr>' },
   },
   opts = {
     provider = 'copilot',
     copilot = {
-      endpoint = 'https://api.githubcopilot.com',
-      model = 'claude-3.7-sonnet',
-      allow_insecure = true, -- Allow insecure server connections
-      timeout = 30000, -- Timeout in milliseconds
-      temperature = 0,
-      max_tokens = 4096,
+      model = 'claude-3.5-sonnet',
+      allow_insecure = true,
+    },
+    vendors = {
+      deepseek_v3 = {
+        __inherited_from = 'openai',
+        api_key_name = 'DEEPSEEK_API_KEY',
+        endpoint = 'https://api.deepseek.com',
+        model = 'deepseek-chat',
+        max_tokens = 4096,
+      },
+      deepseek_r1 = {
+        __inherited_from = 'openai',
+        api_key_name = 'DEEPSEEK_API_KEY',
+        endpoint = 'https://api.deepseek.com',
+        model = 'deepseek-reasoner',
+        max_tokens = 4096,
+        disable_tools = true,
+      },
+    },
     },
   },
   build = 'make',
   dependencies = {
-    'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
