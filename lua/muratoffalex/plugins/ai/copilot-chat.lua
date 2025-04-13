fc5db3ae40897f4d15208753d3d6fd726d3ae117
Alexander Muratov <muratoffalex@gmail.com>
2025-04-13 12:33:50 +0500

    feat(copilot-chat): add openrouter provider and disable github_models

--- lua/muratoffalex/plugins/ai/copilot-chat.lua
+++ lua/muratoffalex/plugins/ai/copilot-chat.lua
@@ -31,6 +31,56 @@ return {
       answer_header = '## Copilot',
       question_header = '## User',
       error_header = '> [!ERROR] Error',
+      providers = {
+        github_models = {
+          disabled = true,
+        },
+        openrouter = {
+          prepare_input = require('CopilotChat.config.providers').copilot.prepare_input,
+          prepare_output = require('CopilotChat.config.providers').copilot.prepare_output,
+
+          get_headers = function()
+            local api_key = assert(os.getenv 'OPENROUTER_API_KEY', 'OPENROUTER_API_KEY env not set')
+            return {
+              Authorization = 'Bearer ' .. api_key,
+              ['Content-Type'] = 'application/json',
+            }
+          end,
+
+          get_models = function(headers)
+            local response, err = require('CopilotChat.utils').curl_get('https://openrouter.ai/api/v1/models', {
+              headers = headers,
+              json_response = true,
+            })
+
+            if err then
+              error(err)
+            end
+
+            local allowed_models = {
+              ['deepseek/deepseek-r1'] = true,
+              ['deepseek/deepseek-chat-v3-0324'] = true,
+            }
+
+            return vim
+              .iter(response.body.data)
+              :filter(function(model)
+                return allowed_models[model.id]
+              end)
+              :map(function(model)
+                return {
+                  id = model.id,
+                  name = model.name,
+                }
+              end)
+              :totable()
+          end,
+
+          get_url = function()
+            return 'https://openrouter.ai/api/v1/chat/completions'
+          end,
+        },
+      },
     }
   end,
   config = function(_, opts)
