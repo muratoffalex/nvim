0a5edb1a5190c88646ea41b301572546e3e8eb0b
Alexander Muratov <muratoffalex@gmail.com>
2025-07-23 15:12:45 +0500

    chore(mcphub): update lualine colors

--- lua/muratoffalex/plugins/extensions/lualine/mcphub-status.lua
+++ lua/muratoffalex/plugins/extensions/lualine/mcphub-status.lua
@@ -31,11 +31,11 @@ return {
 
     local status = vim.g.mcphub_status or 'stopped'
     if status == 'ready' or status == 'restarted' then
-      return { fg = '#50fa7b' } -- Green for connected
+      return "DiagnosticInfo" -- Green for connected
     elseif status == 'starting' or status == 'restarting' then
-      return { fg = '#ffb86c' } -- Orange for connecting
+      return "DiagnosticWarn" -- Orange for connecting
     else
-      return { fg = '#ff5555' } -- Red for error/stopped
+      return "DiagnosticError" -- Red for error/stopped
     end
   end,
 }
