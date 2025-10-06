39130541bf66c4740400932e8ce7bdc6376aebbb
Alexander Muratov <muratoffalex@gmail.com>
2025-10-06 10:18:15 +0500

    feat(scratch): add scratch filetype configuration and keybinds

--- lua/muratoffalex/plugins/scratch.lua
+++ lua/muratoffalex/plugins/scratch.lua
@@ -2,7 +2,23 @@ return {
   'LintaoAmons/scratch.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<leader>sn', '<cmd>ScratchWithName<cr>', desc = 'Scratch new' },
+    { '<leader>sn', '<cmd>Scratch<cr>', desc = 'Scratch new with name' },
+    { '<leader>sN', '<cmd>ScratchWithName<cr>', desc = 'Scratch new' },
     { '<leader>so', '<cmd>ScratchOpen<cr>', desc = 'Scratch open' },
+  },
+  opts = {
+    file_picker = 'snacks',
+    filetypes = { 'go', 'php', 'lua', 'js', 'sh', 'ts' },
+    filetype_details = {
+      go = {
+        requireDir = true,
+        filename = 'main.go',
+        content = { 'package main', '', 'func main() {', '  ', '}' },
+        cursor = {
+          location = { 4, 2 },
+          insert_mode = true,
+        },
+      },
+    },
   },
 }
