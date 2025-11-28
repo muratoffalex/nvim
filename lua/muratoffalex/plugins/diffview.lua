075a300861c98cc23d259504a3cf5ae393aeab61
Alexander Muratov <muratoffalex@gmail.com>
2025-11-29 01:12:25 +0500

    chore(plugins.diffview): add new keymaps

--- lua/muratoffalex/plugins/diffview.lua
+++ lua/muratoffalex/plugins/diffview.lua
@@ -4,7 +4,8 @@ return {
   keys = {
     -- NOTE: frequently used functionality, this more comfortable, but less mnemonic
     -- keymaps starting from <leader> (ex., <leader>D...) and zd not convenient for me
-    { 'ghh', '<cmd>DiffviewFileHistory %<cr>', desc = 'Diffview file history' },
+    { 'ghh', '<cmd>DiffviewFileHistory<cr>', desc = 'Diffview' },
+    { 'ghf', '<cmd>DiffviewFileHistory %<cr>', desc = 'Diffview file history' },
     { 'gho', '<cmd>DiffviewOpen<cr>', desc = 'Diffview open' },
     { 'ghc', '<cmd>DiffviewClose<cr>', desc = 'Diffview close' },
     { 'ght', '<cmd>DiffviewToggleFiles<cr>', desc = 'Diffview toggle files' },
