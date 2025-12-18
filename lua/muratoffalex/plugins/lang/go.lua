1a2a4387146b9407d166fc8b6d1f2de914b2e170
Alexander Muratov <muratoffalex@gmail.com>
2025-12-18 15:21:04 +0500

    feat(go): add conf for dap

--- lua/muratoffalex/plugins/lang/go.lua
+++ lua/muratoffalex/plugins/lang/go.lua
@@ -11,6 +11,27 @@ return {
       lsp_inlay_hints = {
         enable = false,
       },
+      dap_debug_gui = {
+        layouts = {
+          {
+            elements = {
+              { id = 'stacks', size = 0.25 },
+              { id = 'scopes', size = 0.25 },
+              { id = 'breakpoints', size = 0.25 },
+              { id = 'watches', size = 0.25 },
+            },
+            position = 'right',
+            size = 40,
+          },
+          {
+            elements = {
+              { id = 'repl', size = 1 },
+            },
+            position = 'bottom',
+            size = 10,
+          },
+        },
+      },
     },
     config = function(_, opts)
       require('go').setup(opts)
@@ -26,9 +47,10 @@ return {
       keymap('lgA', '<cmd>GoAltV!<cr>', 'Open test/file in vsplit')
       keymap('lgt', '<cmd>GoTest<cr>', 'Run tests')
       keymap('lgT', ':GoTest', 'Run tests with args')
+      keymap('lgT', '<cmd>GoBreakToggle<cr>', 'Toggle breakpoint')
 
       local format_sync_grp = vim.api.nvim_create_augroup('goimports', {})
-      vim.api.nvim_create_autocmd({'BufWritePre','BufWriteCmd'}, {
+      vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufWriteCmd' }, {
         pattern = '*.go',
         callback = function()
           require('go.format').goimports()
@@ -36,9 +58,9 @@ return {
         group = format_sync_grp,
       })
 
-      require('which-key').add({
+      require('which-key').add {
         { 'lg', group = 'Golang' },
-      })
+      }
     end,
   },
 }
